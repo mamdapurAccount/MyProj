af492eada708573785dc7bf4280056e2a5e832e1
Manoj <manatharv@gmail.com>
2019-10-26 09:50:52 +0530

    Namrata Commit

---
+++ MyFirst/src/com/aditi/Account.java
@@ -0,0 +1,21 @@
+package com.aditi;
+
+public class Account {
+
+	
+	int acno;
+	String acname;
+	public int getAcno() {
+		return acno;
+	}
+	public void setAcno(int acno) {
+		this.acno = acno;
+	}
+	public String getAcname() {
+		return acname;
+	}
+	public void setAcname(String acname) {
+		this.acname = acname;
+	}
+	
+}
